1dfe4037182c6baf42dd0124f8c356711abe2a35
taicheeze <tiebo@ucsb.edu>
2024-07-25 09:35:20 -0700

    added report for july 2024 and split report generation to absolute and relative

--- tag_analysis/tag_report.js
+++ tag_analysis/tag_report.js
@@ -1,30 +1,25 @@
-
-import siteTags from './site_tags.json' assert {type: 'json'};
-import fs from 'fs';
+const siteTags = require("./site_tags.json");
+const fs = require("fs");
+const { parse } = require("json2csv");
 const tagsObject = siteTags.tag;
-const tagsSimplified = tagsObject.map( (tag) => ({  
-    title: tag.title,
-    pages: tag.pages,
-    id: tag["@id"],
+const tagsSimplified = tagsObject.map((tag) => ({
+  title: tag.title,
+  pages: tag.pages,
+  id: tag["@id"],
 }));
 
-const tagsNoEmpty = tagsSimplified.filter((tag)=>tag.pages["@count"] > 1);
-const tagsSimplifiedFiltered = tagsNoEmpty.filter((tag)=> {
-    return tag.pages.page.some((page)=> page["uri.ui"].includes('/tc/'))
+const tagsNoEmpty = tagsSimplified.filter((tag) => tag.pages["@count"] > 1);
+const tagsSimplifiedFiltered = tagsNoEmpty.filter((tag) => {
+  return tag.pages.page.some((page) => page["uri.ui"].includes("/tc/"));
 });
 
-const tagsOnly = tagsSimplifiedFiltered.map( (tag) => ({  
-    title: tag.title,
-    id: tag.id
+const tagsOnly = tagsSimplifiedFiltered.map((tag) => ({
+  title: tag.title,
+  id: tag.id,
+  pages: tag.pages["@count"],
 }));
-
-function convertToCSV(arr) {
-    const array = [Object.keys(arr[0])].concat(arr)
-  
-    return array.map(it => {
-      return Object.values(it).toString()
-    }).join('\n')
-  }
+const csvData = parse(tagsOnly);
+const reportDate = "2023Dec19";
 
 // const tutorialsOnly = tagsSimplifiedFiltered.filter((tag)=>tag.url.includes('/tutorials/'));
 // const faqsOnly = tagsSimplifiedFiltered.filter((tag)=>tag.url.includes('/faq/'));
@@ -33,27 +28,21 @@ console.log(`all articles: ${tagsSimplifiedFiltered.length}`);
 // console.log(`tutorials: ${tutorialsOnly.length}`);
 // console.log(`faqs: ${faqsOnly.length}`);
 
-const data = JSON.stringify(tagsOnly);
-const csvData = convertToCSV(tagsOnly);
-
 //const tutorialData = JSON.stringify(tutorialsOnly);
 //const faqData = JSON.stringify(faqsOnly);
 
 // write JSON string to a file
 
-fs.writeFile('tagsOnly.json', data, (err) => {
+fs.writeFile(
+  `./reports/tag_reports/MT-Tags-report-${reportDate}.csv`,
+  csvData,
+  (err) => {
     if (err) {
-        throw err;
+      throw err;
     }
-    console.log("JSON data is saved.");
-});
-
-fs.writeFile('tagsOnly.csv', csvData, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("JSON data is saved.");
-});
+    console.log("report saved.");
+  }
+);
 // fs.writeFile('tutorials.json', tutorialData, (err) => {
 //     if (err) {
 //         throw err;
